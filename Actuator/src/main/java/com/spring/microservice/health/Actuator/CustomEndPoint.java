ab6e62db7b83d81ff051a05835b634557c985628
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-28 20:21:46 +0530

    Custom Endpoint

---
+++ Actuator/src/main/java/com/spring/microservice/health/Actuator/CustomEndPoint.java
@@ -0,0 +1,26 @@
+package com.spring.microservice.health.Actuator;
+/*
+
+@Component
+public class CustomEndPoint implements Endpoint<String>{
+    @Override
+    public String getId() {
+        return "youtube";
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isSensitive() {
+        return false;
+    }
+
+    @Override
+    public String invoke() {
+        return "Hello Youtube";
+    }
+}
+*/
