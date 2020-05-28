80a71ff9e7d7572ceab10b2834c31f25f9592a0e
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-28 18:20:28 +0530

    Actuator

---
+++ Actuator/src/main/java/com/spring/microservice/health/Actuator/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.spring.microservice.health.Actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorApplication.class, args);
+	}
+
+}
